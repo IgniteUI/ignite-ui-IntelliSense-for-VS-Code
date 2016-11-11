b4236e55b6963937931d139ae1aec034a10f59dc
Vasya Kacheshmarova <vasq1989@gmail.com>
2016-11-11 16:15:58 +0200

    Ignite UI IntelliSense extension for VS Code
    Ignite UI IntelliSense extension for VS Code

---
+++ README.md
@@ -0,0 +1,36 @@
+﻿# IgniteUIIntelliSense for Visual Studio Code
+
+This extension adds IntelliSense for IgniteUI controls to your visual Visual Studio Code.
+
+
+![](images/IntelliSense_Screenshot.png)
+
+
+#Getting Started
+1. This extension creates a folder named 'igniteui' in the root directory of your application. jQuery and IgniteUI TypeScript definitions are downloaded in this folder.
+2. Include references to this files in your application.
+ ```html
+ /// <reference path="../igniteui/jquery.d.ts" />
+/// <reference path="../igniteui/igniteui.d.ts" />
+ ```
+3. Voila - ItelliSense for Ignite UI controls is avaliable in your application.
+
+#Running in GitHub
+To tun the extension, you need [Node.js](https://nodejs.org/en/) installed
+1. git clone https://github.com/IgniteUI/ignite-ui-IntelliSense-for-VS-Code.git
+2. npm start
+3. Press F5 to open a new VS Code window with your extension loaded.
+4. Open the application that you want to enable the extension for.
+5. Press Ctrl+Shift+P and type IgniteUI IntelliSense
+6. Include references to the files in the newly created folder.
+
+ ```html
+ /// <reference path="../igniteui/jquery.d.ts" />
+/// <reference path="../igniteui/igniteui.d.ts" />
+ ```
+
+
+
+
+-----------------------------------------------------------------------------------------------------------
+
---
+++ src/extension.ts
@@ -0,0 +1,49 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+import * as vscode from 'vscode';
+
+var fs = require("fs"),
+    path = require("path"),
+    root = vscode.workspace.rootPath,
+    https = require('https'),
+    dir = root + "\\igniteui";
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    // This lines of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "IgniteUIIntelliSense" is now active!');
+    checkDirectory(dir, function(){
+        fs.appendFile(dir+"\\igniteui.d.ts", "");
+        fs.appendFile(dir+"\\jquery.d.ts", "");
+        var fileIgniteUITypings = fs.createWriteStream(dir+"\\igniteui.d.ts"),
+             fileJQueryTypings = fs.createWriteStream(dir+"\\jquery.d.ts");
+            
+        var request = https.get("https://raw.githubusercontent.com/IgniteUI/igniteui-angular2/master/src/igniteui.d.ts", function(response) {
+            response.pipe(fileIgniteUITypings);
+        });
+        var request2 = https.get("https://raw.githubusercontent.com/IgniteUI/igniteui-angular2/master/src/jquery.d.ts", function(response) {
+            response.pipe(fileJQueryTypings);
+        });
+    });
+    // The command has been defined in the package.json file
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+        // Display a message box to the user
+        vscode.window.showInformationMessage('IgniteUI IntelliSense');
+    });
+
+    context.subscriptions.push(disposable);
+}
+function checkDirectory(directory, callback) {  
+  fs.stat(directory, function(err, stats) {
+    //Check if error defined and the error code is "not exists"
+    if (err && err.errno === -4058) {
+      //Create the directory, call the callback.
+      fs.mkdir(directory, callback);
+    } else {
+      //just in case there was a different error:
+      callback(err)
+    }
+  }
+
